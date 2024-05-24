7141ff9509ed9dd0fc905b41aed5f4ff3e616bf0
seongho-git <klue980@gmail.com>
2024-05-24 10:07:28 +0000

    add fp2.py

--- llama/run_batch_fp2.py
+++ llama/run_batch_fp2.py
@@ -12,26 +12,26 @@ import subprocess
 
 # sweep parameter
 # --batch_size 512 --max_input_len 64 --output_len 512
-max_ite = 2 # if hf : 1, trt :1
-list_batch_size = [1] # [1, 8, 64]
+max_ite = 1 # if hf : 1, trt :1
+list_batch_size = [1, 64] # [1, 8, 64]
 # batch_size = 1
 list_max_input_len = [64] # [1, 4, 16, 256]
 # max_input_len = 512
-list_output_len = [512] # [8, 64, 512]
+list_output_len = [8, 64, 512] # [8, 64, 512]
 
 # iteration script
 # --test_trt_llm --test_hf
 # change 3 metrics
 for batch_size in list_batch_size:
     for max_input_len in list_max_input_len: 
-        build_command = f"trtllm-build --checkpoint_dir /workspace/TensorRT-LLM/examples/llama/check/hf/3-8b/bf16 \
-                            --gemm_plugin bfloat16 \
-                            --gpt_attention_plugin bfloat16 \
+        build_command = f"trtllm-build --checkpoint_dir /workspace/TensorRT-LLM/examples/llama/check/hf/2b/fp16 \
+                            --gemm_plugin float16 \
+                            --gpt_attention_plugin float16 \
                             --max_batch_size {batch_size} \
                             --max_input_len {max_input_len} \
                             --max_output_len 1024 \
-                            --lookup_plugin bfloat16 \
-                            --output_dir /workspace/TensorRT-LLM/examples/llama/trt-engine/hf/3-8b/bf16"
+                            --lookup_plugin float16 \
+                            --output_dir /workspace/TensorRT-LLM/examples/llama/trt-engine/hf/2b/fp16"
         try:
             print(build_command)
             subprocess.run(build_command, shell=True, check=True)
@@ -42,9 +42,9 @@ for batch_size in list_batch_size:
             base_command = f"nsys profile --wait all -t cuda,nvtx,cudnn,cublas -f true \
                             --stats true -w true -o /workspace/TensorRT-LLM/examples/llama/NSYS/{ex_name}.nsys-rep \
                             python3 /workspace/TensorRT-LLM/examples/summarize.py --test_trt_llm \
-                            --hf_model_dir /workspace/TensorRT-LLM/examples/llama/Meta-Llama-3-8B \
-                            --data_type bf16 \
-                            --engine_dir /workspace/TensorRT-LLM/examples/llama/trt-engine/hf/3-8b/bf16 \
+                            --hf_model_dir /workspace/TensorRT-LLM/examples/llama/Llama-2-7b-hf \
+                            --data_type fp16 \
+                            --engine_dir /workspace/TensorRT-LLM/examples/llama/trt-engine/hf/2b/fp16 \
                             --batch_size {batch_size} \
                             --max_input_length {max_input_len} \
                             --output_len {output_len} \
